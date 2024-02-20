26490c7723b5f025bdf3c2ea7b72ed29714ad009
João Felipe <joaofelipejflj16@gmail.com>
2024-02-20 20:58:15 -0300

    Criado a classe carro

---
+++ aula1.py
@@ -0,0 +1,19 @@
+# # Exemplo de declaração de uma classe em Python
+# class Carro:
+#     # Método construtor
+#     def __init__(self, marca, modelo, ano):
+#         self.marca = marca
+#         self.modelo = modelo
+#         self.ano = ano
+
+# # Método para imprimir informações do carro
+#     def imprimir_info(self):
+#         print(f"Marca: {self.marca}, Modelo: {self.modelo}: Ano: {self.ano}")
+
+# # Criando objetos da classe Carro     
+# carro1 = Carro("Ford", "Fiesta", 2020) 
+# carro2 = Carro("Chevrolet", "Onix", 2019)
+
+# # Chamando método para imprimir informações dos carros
+# carro1.imprimir_info()
+# carro2.imprimir_info()
